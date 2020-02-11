319d1f1b64c1aeff98524f23ebdcc11e7c2f027f
Hannah O'Sullivan <hjosullivan123@gmail.com>
2020-02-11 16:56:09 +0200

    Edit maniot.cpp

--- manihot-sf/maniot.cpp
+++ manihot-sf/maniot.cpp
@@ -44,11 +44,11 @@ inline double sign(Uniform& u)
 
 //Branching angles in the main branch
 const double MIN_PITCH = 40.0*PI_VALUE/180.0;
-const double MAX_PITCH = 80.0*PI_VALUE/180.0;
-//const double MIN_TURN = 30.0*PI_VALUE/180.0;
-//const double MAX_TURN = 60.0*PI_VALUE/180.0;
-//const double MIN_FORK = 5.0*PI_VALUE/180.0;
-//const double MAX_FORK = 10.0*PI_VALUE/180.0;
+const double MAX_PITCH = 50.0*PI_VALUE/180.0;
+const double MIN_TURN = 30.0*PI_VALUE/180.0;
+const double MAX_TURN = 60.0*PI_VALUE/180.0;
+const double MIN_FORK = 5.0*PI_VALUE/180.0;
+const double MAX_FORK = 10.0*PI_VALUE/180.0;
 //const double MIN_FORK_AU2 = 0.0*PI_VALUE/180.0;
 //const double MAX_FORK_AU2 = 5.0*PI_VALUE/180.0;
 //const double MIN_PIVOT = 0.0*PI_VALUE/180.0;
@@ -61,8 +61,8 @@ const double MAX_PITCH = 80.0*PI_VALUE/180.0;
 //const double P_AU2 = 0.3;
 //Segment default dimenstions
 const double LSEG = 0.40;
-//const double LA2 = 0.20;
-//const double LA3 = 0.10;
+const double LA2 = 0.20;
+const double LA3 = 0.10;
 const double RA1 = 0.004;
 //const double RAU2 = 0.002;
 //const double RA2 = 0.002;
@@ -73,10 +73,10 @@ const double RA1 = 0.004;
 //Axis types
 const double A1 = 1.0;
 const double A2 = 2.0;
-//const double A3 = 3.0;
-//const double A4 = 4.0;
+const double A3 = 3.0;
+const double A4 = 4.0;
 //const double AU2 = 4.0;
-//const double THICK = 0.01;
+const double THICK = 0.01;
 //The declare the modules your need 
 //Fd(length,radius,radius_top,axis_type)
   const ModuleIdType Fd_id = 2;
@@ -103,25 +103,50 @@ void Start()
   u.init(init);
   double pitch1 = ba(MIN_PITCH,MAX_PITCH,u);
   double pitch2 = ba(MIN_PITCH,MAX_PITCH,u);
-  { Produce((ModuleIdType)(Fd_id));Produce((double)(LSEG));Produce((double)(RA1));Produce((double)(RA1));Produce((double)(A1));Produce((ModuleIdType)(Fd_id)); Produce((ModuleIdType)(SB_id)); Produce((ModuleIdType)(Pitch_id));Produce((double)(pitch1));Produce((ModuleIdType)(Pitch_id));  Produce((ModuleIdType)(B_id));Produce((double)(A2));Produce((double)(1));Produce((double)(1));Produce((double)(ALIVE));Produce((ModuleIdType)(B_id));Produce((ModuleIdType)(EB_id)); 
-                              Produce((ModuleIdType)(SB_id)); Produce((ModuleIdType)(Roll_id));Produce((double)(PI_VALUE));Produce((ModuleIdType)(Roll_id)); Produce((ModuleIdType)(Pitch_id));Produce((double)(pitch2));Produce((ModuleIdType)(Pitch_id));  Produce((ModuleIdType)(B_id));Produce((double)(A2));Produce((double)(1));Produce((double)(1));Produce((double)(ALIVE));Produce((ModuleIdType)(B_id));Produce((ModuleIdType)(EB_id));
-                                   Produce((ModuleIdType)(Roll_id));Produce((double)(PI_VALUE/2.0));Produce((ModuleIdType)(Roll_id)); Produce((ModuleIdType)(B_id));Produce((double)(A1));Produce((double)(1));Produce((double)(1));Produce((double)(ALIVE));Produce((ModuleIdType)(B_id));}
+  { Produce((ModuleIdType)(Fd_id));Produce((double)(LSEG));Produce((double)(RA1));Produce((double)(RA1));Produce((double)(A1));Produce((ModuleIdType)(Fd_id)); Produce((ModuleIdType)(SB_id)); Produce((ModuleIdType)(Pitch_id));Produce((double)(pitch1));Produce((ModuleIdType)(Pitch_id));  Produce((ModuleIdType)(B_id));Produce((double)(A2));Produce((double)(1));Produce((double)(1));Produce((double)(ALIVE));Produce((double)(0));Produce((ModuleIdType)(B_id));Produce((ModuleIdType)(EB_id)); 
+                              Produce((ModuleIdType)(SB_id)); Produce((ModuleIdType)(Roll_id));Produce((double)(PI_VALUE));Produce((ModuleIdType)(Roll_id)); Produce((ModuleIdType)(Pitch_id));Produce((double)(pitch2));Produce((ModuleIdType)(Pitch_id));  Produce((ModuleIdType)(B_id));Produce((double)(A2));Produce((double)(1));Produce((double)(1));Produce((double)(ALIVE));Produce((double)(0));Produce((ModuleIdType)(B_id));Produce((ModuleIdType)(EB_id));
+                                   Produce((ModuleIdType)(Roll_id));Produce((double)(PI_VALUE/2.0));Produce((ModuleIdType)(Roll_id)); Produce((ModuleIdType)(B_id));Produce((double)(A1));Produce((double)(1));Produce((double)(1));Produce((double)(DEAD));Produce((double)(0));Produce((ModuleIdType)(B_id));}
 }
 
-
-void _P1(double axis_type,double age,double phys_age,double status)
+//Bud produces tree segments and lateral buds
+void _P1(double axis_type,double age,double phys_age,double status,double pitch_angle)
 {
   double ran = u(1);
   double pitch1 = ba(MIN_PITCH,MAX_PITCH,u);
   double pitch2 = ba(MIN_PITCH,MAX_PITCH,u);
-  { Produce((ModuleIdType)(Fd_id));Produce((double)(LSEG));Produce((double)(RA1));Produce((double)(RA1));Produce((double)(A1));Produce((ModuleIdType)(Fd_id)); Produce((ModuleIdType)(SB_id)); Produce((ModuleIdType)(Pitch_id));Produce((double)(pitch1));Produce((ModuleIdType)(Pitch_id));  Produce((ModuleIdType)(B_id));Produce((double)(A2));Produce((double)(1));Produce((double)(1));Produce((double)(ALIVE));Produce((ModuleIdType)(B_id));Produce((ModuleIdType)(EB_id)); 
-                              Produce((ModuleIdType)(SB_id)); Produce((ModuleIdType)(Roll_id));Produce((double)(PI_VALUE));Produce((ModuleIdType)(Roll_id)); Produce((ModuleIdType)(Pitch_id));Produce((double)(pitch2));Produce((ModuleIdType)(Pitch_id));  Produce((ModuleIdType)(B_id));Produce((double)(A2));Produce((double)(1));Produce((double)(1));Produce((double)(ALIVE));Produce((ModuleIdType)(B_id));Produce((ModuleIdType)(EB_id));
-                                   Produce((ModuleIdType)(Roll_id));Produce((double)(PI_VALUE/2.0));Produce((ModuleIdType)(Roll_id)); Produce((ModuleIdType)(B_id));Produce((double)(A1));Produce((double)(1));Produce((double)(1));Produce((double)(DEAD));Produce((ModuleIdType)(B_id));}
+  if (status==ALIVE && axis_type == A1){
+      { Produce((ModuleIdType)(Fd_id));Produce((double)(LSEG));Produce((double)(RA1));Produce((double)(RA1));Produce((double)(A1));Produce((ModuleIdType)(Fd_id)); Produce((ModuleIdType)(SB_id)); Produce((ModuleIdType)(Pitch_id));Produce((double)(pitch1));Produce((ModuleIdType)(Pitch_id));  Produce((ModuleIdType)(B_id));Produce((double)(A2));Produce((double)(1));Produce((double)(1));Produce((double)(ALIVE));Produce((double)(pitch_angle));Produce((ModuleIdType)(B_id));Produce((ModuleIdType)(EB_id)); 
+                                  Produce((ModuleIdType)(SB_id)); Produce((ModuleIdType)(Roll_id));Produce((double)(PI_VALUE));Produce((ModuleIdType)(Roll_id)); Produce((ModuleIdType)(Pitch_id));Produce((double)(pitch2));Produce((ModuleIdType)(Pitch_id));  Produce((ModuleIdType)(B_id));Produce((double)(A2));Produce((double)(1));Produce((double)(1));Produce((double)(ALIVE));Produce((double)(pitch_angle));Produce((ModuleIdType)(B_id));Produce((ModuleIdType)(EB_id));
+                    Produce((ModuleIdType)(Roll_id));Produce((double)(PI_VALUE/2.0));Produce((ModuleIdType)(Roll_id)); Produce((ModuleIdType)(B_id));Produce((double)(A1));Produce((double)(1));Produce((double)(1));Produce((double)(DEAD));Produce((double)(pitch_angle));Produce((ModuleIdType)(B_id));}
+   }
+   else if (status == ALIVE && axis_type == A2){
+   	double max_pitch_angle = MAX_FORK;
+        if (pitch_angle >= 85.0*PI_VALUE/180.0){
+            max_pitch_angle = 0.0;
+	}
+        double new_pitch_angle = pitch_angle + max_pitch_angle;
+   	{ Produce((ModuleIdType)(Fd_id));Produce((double)(LA2));Produce((double)( RA1));Produce((double)( RA1));Produce((double)( axis_type));Produce((ModuleIdType)(Fd_id)); Produce((ModuleIdType)(SB_id)); Produce((ModuleIdType)(Turn_id));Produce((double)(MIN_TURN));Produce((ModuleIdType)(Turn_id)); Produce((ModuleIdType)(Pitch_id));Produce((double)(max_pitch_angle));Produce((ModuleIdType)(Pitch_id));Produce((ModuleIdType)(B_id));Produce((double)(A3));Produce((double)(1));Produce((double)(1));Produce((double)(ALIVE));Produce((double)(new_pitch_angle));Produce((ModuleIdType)(B_id));Produce((ModuleIdType)(EB_id));
+			      	       Produce((ModuleIdType)(SB_id)); Produce((ModuleIdType)(Turn_id));Produce((double)(-MIN_TURN));Produce((ModuleIdType)(Turn_id)); Produce((ModuleIdType)(Pitch_id));Produce((double)(-max_pitch_angle));Produce((ModuleIdType)(Pitch_id)); Produce((ModuleIdType)(B_id));Produce((double)(A3));Produce((double)(1));Produce((double)(1));Produce((double)(ALIVE));Produce((double)(new_pitch_angle));Produce((ModuleIdType)(B_id));Produce((ModuleIdType)(EB_id));
+				       Produce((ModuleIdType)(B_id));Produce((double)(axis_type));Produce((double)(1));Produce((double)(1));Produce((double)(DEAD));Produce((double)(new_pitch_angle));Produce((ModuleIdType)(B_id));}
+   }
+   else if (status == ALIVE && axis_type > A2){
+   	double max_pitch_angle = MAX_FORK;
+	if (pitch_angle >= 85.0*PI_VALUE/180.0){
+	   max_pitch_angle = 0.0;
+	}
+	double new_pitch_angle = pitch_angle + max_pitch_angle;
+   	{ Produce((ModuleIdType)(Fd_id));Produce((double)(LA3));Produce((double)( RA1));Produce((double)( RA1));Produce((double)( axis_type));Produce((ModuleIdType)(Fd_id)); Produce((ModuleIdType)(SB_id)); Produce((ModuleIdType)(Turn_id));Produce((double)(MIN_TURN));Produce((ModuleIdType)(Turn_id)); Produce((ModuleIdType)(Pitch_id));Produce((double)(-max_pitch_angle));Produce((ModuleIdType)(Pitch_id)); Produce((ModuleIdType)(B_id));Produce((double)(A4));Produce((double)(1));Produce((double)(1));Produce((double)(ALIVE));Produce((double)(new_pitch_angle));Produce((ModuleIdType)(B_id)); Produce((ModuleIdType)(EB_id));
+   	   	   	     		Produce((ModuleIdType)(SB_id)); Produce((ModuleIdType)(Turn_id));Produce((double)(-MIN_TURN));Produce((ModuleIdType)(Turn_id)); Produce((ModuleIdType)(Pitch_id));Produce((double)(-max_pitch_angle));Produce((ModuleIdType)(Pitch_id)); Produce((ModuleIdType)(B_id));Produce((double)(A4));Produce((double)(1));Produce((double)(1));Produce((double)(ALIVE));Produce((double)(new_pitch_angle));Produce((ModuleIdType)(B_id)); Produce((ModuleIdType)(EB_id));
+					Produce((ModuleIdType)(B_id));Produce((double)(axis_type));Produce((double)(1));Produce((double)(1));Produce((double)(DEAD));Produce((double)(new_pitch_angle));Produce((ModuleIdType)(B_id));}
+   }
+   else{
+   { Produce((ModuleIdType)(B_id));Produce((double)(axis_type));Produce((double)(age));Produce((double)(phys_age));Produce((double)(status));Produce((double)(pitch_angle));Produce((ModuleIdType)(B_id));}
+   }
 }
 
 
 //Left hand and right hand context or Bud
-    void _P2(double THICK,double c,double axis_type,double age,double phys_age,double status,double l1,double r1,double rtop1,double axis_type1)
+    void _P2(double THICK,double c,double axis_type,double age,double phys_age,double status,double pitch_angle,double l1,double r1,double rtop1,double axis_type1)
 {
 if (THICK > 0.0)
 { Produce((ModuleIdType)(Fd_id));Produce((double)(l1));Produce((double)(THICK));Produce((double)(THICK));Produce((double)(axis_type1));Produce((ModuleIdType)(Fd_id));Produce((ModuleIdType)(D_id));Produce((double)(THICK/2.0));Produce((double)(1.0));Produce((ModuleIdType)(D_id));}
@@ -166,7 +191,10 @@ memcpy(&p2, pArg, sizeof(double));
 pArg += sizeof(double);
 double p3;
 memcpy(&p3, pArg, sizeof(double));
-_P1(p0,p1,p2,p3);
+pArg += sizeof(double);
+double p4;
+memcpy(&p4, pArg, sizeof(double));
+_P1(p0,p1,p2,p3,p4);
 }
 
 
@@ -191,10 +219,10 @@ memcpy(&p4, pArg, sizeof(double));
 pArg += sizeof(double);
 double p5;
 memcpy(&p5, pArg, sizeof(double));
-pArg = pCD->RCntxt.pArg(0);
+pArg += sizeof(double);
 double p6;
 memcpy(&p6, pArg, sizeof(double));
-pArg += sizeof(double);
+pArg = pCD->RCntxt.pArg(0);
 double p7;
 memcpy(&p7, pArg, sizeof(double));
 pArg += sizeof(double);
@@ -203,7 +231,10 @@ memcpy(&p8, pArg, sizeof(double));
 pArg += sizeof(double);
 double p9;
 memcpy(&p9, pArg, sizeof(double));
-_P2(p0,p1,p2,p3,p4,p5,p6,p7,p8,p9);
+pArg += sizeof(double);
+double p10;
+memcpy(&p10, pArg, sizeof(double));
+_P2(p0,p1,p2,p3,p4,p5,p6,p7,p8,p9,p10);
 }
 
 
@@ -316,7 +347,7 @@ static const ModuleData moduleData[] =
 { "SB",  0 },
 { "EB",  0 },
 { "Fd", sizeof(double)+sizeof(double)+sizeof(double)+sizeof(double)},
-{ "B", sizeof(double)+sizeof(double)+sizeof(double)+sizeof(double)},
+{ "B", sizeof(double)+sizeof(double)+sizeof(double)+sizeof(double)+sizeof(double)},
 { "Pitch", sizeof(double)},
 { "Turn", sizeof(double)},
 { "Roll", sizeof(double)},
